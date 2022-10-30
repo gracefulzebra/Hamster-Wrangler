4ef4843223896003e764e672ab8342c44e1d6fad
SIvory1 <77367707+SIvory1@users.noreply.github.com>
2022-10-30 16:54:32 +0000

    added free move camera and grid (#2)
    added free moving camera and grid tilemap, started setting up grid structure and was messing around with painting on the grid

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Header("Camera")]
+    public float mouseSens;
+    public Transform cameraParent;
+    float rotationX = 0f;
+    float rotationY = 0f;
+
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float verticalSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotate();
+        CameraMovements();
+    }
+
+
+    void CameraMovements()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            transform.position += transform.up * verticalSpeed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+
+            transform.position -= transform.up * verticalSpeed * Time.deltaTime;
+
+        }
+        transform.position += transform.forward * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position += transform.right * moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+    }
+
+    void CameraRotate()
+    {
+
+        rotationX += Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        rotationY += Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+    }
+}
