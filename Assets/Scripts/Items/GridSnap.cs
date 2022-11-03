3538556fdbbb9f540e2e242d514e8a3eff8fd54f
SIvory1 <SIVORY200@caledonian.ac.uk>
2022-11-03 14:01:25 +0000

    Working on snapping to grid
    snaps to grid pieces, can move objects using mouse curser, doesnt work well and can move objects off grid and it still works. perspective messes with movement with mouse cursors

---
+++ Assets/Scripts/Items/GridSnap.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSnap : MonoBehaviour
+{
+
+    private Grid grid;
+    bool onGround;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = Grid.FindObjectOfType<Grid>();
+        onGround = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onGround)
+        {
+            Vector3Int cp = grid.LocalToCell(transform.localPosition);
+            transform.localPosition = grid.GetCellCenterLocal(cp);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Tile_8x8")
+        {
+            onGround = true;
+            print("on");
+        }
+        else
+        {
+            onGround = false;
+            print("off");
+        }
+    }
+}
