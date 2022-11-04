d8c12f2941284e41b0fbed49f507d168f92379ff
SIvory1 <SIVORY200@caledonian.ac.uk>
2022-11-04 15:20:59 +0000

    smoother movement snap
    trying to get the movement snap to be smoother right now, cant get it working. the thought process is that i want it so that when you move the cursor up (yaxis) is moves the object on the zaxis, this will make it less glitchy at weird camera angles

--- Assets/Scripts/Items/GizmosController.cs
+++ Assets/Scripts/Items/GizmosController.cs
@@ -1,6 +1,9 @@
+using Newtonsoft.Json.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(BoxCollider))]
 
@@ -9,6 +12,8 @@ public class GizmosController : MonoBehaviour
 
     private Vector3 screenPoint;
     private Vector3 offset;
+    private GameObject isDrag;
+
 
     void OnMouseDown()
     {
@@ -20,10 +25,45 @@ public class GizmosController : MonoBehaviour
 
     void OnMouseDrag()
     {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 tempVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+
+        Vector3 curScreenPoint = new Vector3(tempVec.x, tempVec.y , screenPoint.z);
+
+       // Vector3 tempVec = new Vector3(curScreenPoint.x, Input.mousePosition.z, curScreenPoint);
 
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
         transform.position = curPosition;
-
     }
 }
+
+
+        /*
+
+
+
+          private Vector3 mOffset;
+        private float mZCoord;
+
+
+        private void OnMouseDrag()
+        {
+            transform.position = GetMouseWorldPos() + mOffset; 
+        }
+
+        private void OnMouseDown()
+        {
+            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+            mOffset = gameObject.transform.position - GetMouseWorldPos(); 
+        }
+
+        Vector3 GetMouseWorldPos()
+        {
+            Vector3 mousePoint = Input.mousePosition;
+
+            mousePoint.z = mZCoord;
+
+            return Camera.main.ScreenToWorldPoint(mousePoint);
+        }
+        */
+    
--- Assets/Scripts/Items/GridSnap.cs
+++ Assets/Scripts/Items/GridSnap.cs
@@ -5,37 +5,18 @@ using UnityEngine;
 public class GridSnap : MonoBehaviour
 {
     
+    public GridGenerator gridGen;
     private Grid grid;
-    bool onGround;
-   
     // Start is called before the first frame update
     void Start()
     {
         grid = Grid.FindObjectOfType<Grid>();
-        onGround = true;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (onGround)
-        {
+    {  
             Vector3Int cp = grid.LocalToCell(transform.localPosition);
-            transform.localPosition = grid.GetCellCenterLocal(cp);
-        }
-    }
-
-    public void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "Tile_8x8")
-        {
-            onGround = true;
-            print("on");
-        }
-        else
-        {
-            onGround = false;
-            print("off");
-        }
+            transform.localPosition = grid.GetCellCenterLocal(cp);      
     }
 }
