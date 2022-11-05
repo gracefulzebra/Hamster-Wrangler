473e8adc38c45bb460d29b5d5320f086bc01728f
SIvory1 <SIVORY200@caledonian.ac.uk>
2022-11-05 22:11:24 +0000

    made snap to grid script more readable

--- Assets/Scripts/Items/SnapToGrid.cs
+++ Assets/Scripts/Items/SnapToGrid.cs
@@ -9,41 +9,35 @@ public class SnapToGrid : MonoBehaviour
     public GameObject gameObject;
     Vector3 adjustOffset = new Vector3(0f, 0.26f, 0f);
 
+
     public void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
-            Ray mousePos = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(mousePos, out hit))
-            {
-                // gameObject = hit.transform.gameObject;
-                print("john");
-                Node nodehit = gridRef.GetNodeFromWorldPoint(hit.point);
-                if (nodehit.walkable)
-                {
-                    gameObject.transform.position = nodehit.worldPosition + adjustOffset;
-                }
-            }
+            nodeCheck();
         }
-  
     }
     
     void OnMouseDown()
-    {
-    
+    {  
     }
-
 
     void OnMouseDrag()
     {
-       // nodeCheck();
     }
-    
 
     void nodeCheck()
     {
-   
+        RaycastHit hit;
+        Ray mousePos = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(mousePos, out hit))
+        {
+            Node nodehit = gridRef.GetNodeFromWorldPoint(hit.point);
+            if (nodehit.walkable)
+            {
+                gameObject.transform.position = nodehit.worldPosition + adjustOffset;
+            }
+        }
     }
 }
 
