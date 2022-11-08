bb7d8d0223c1313baa0139c12881d712252d8c5c
SIvory1 <SIVORY200@caledonian.ac.uk>
2022-11-08 15:22:08 +0000

    added fixed camera
    added the fixed camera in main scene, press a directional key to switch to next view

---
+++ Assets/Scripts/Gameplay/FixedCam.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCam : MonoBehaviour
+{
+
+    public List<Transform> cameraPos = new List<Transform>();
+    int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // spawns camera at first camera position
+        transform.position = cameraPos[0].transform.position;
+        transform.rotation = cameraPos[0].transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // when you press direction key 
+        if (Input.GetKeyDown(KeyCode.A) | Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // checks if past bounds of list 
+            if (count == cameraPos.Count - 1)
+            {
+                // if it is resets the counter so it continues to next pos without having to go all the way back round manuelly
+                count = 0;
+                transform.position = cameraPos[count].transform.position;
+                transform.rotation = cameraPos[count].transform.rotation;
+            }
+            else
+            {
+                // changes the camera pos and rot to view the scene
+                transform.position = cameraPos[count + 1].transform.position;
+                transform.rotation = cameraPos[count + 1].transform.rotation;
+                count += 1;
+            }
+        }
+        // when you press direction key 
+        if (Input.GetKeyDown(KeyCode.D) | Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // checks if past bounds of list 
+            if (count == 0)
+            {
+                // if it is resets the counter so it continues to next pos without having to go all the way back round manuelly (MANUEL-ly??? MANIX MANUEL VAL GAMER???? MAUEL LOVE YOU???)
+                count = 3;
+                transform.position = cameraPos[count].transform.position;
+                transform.rotation = cameraPos[count].transform.rotation;
+            }
+            else
+            {
+                // changes the camera pos and rot to view the scene
+                transform.position = cameraPos[count - 1].transform.position;
+                transform.rotation = cameraPos[count - 1].transform.rotation;
+                count -= 1;
+            }
+
+        }
+    }
+}
--- Assets/Scripts/Items/Fan.cs
+++ Assets/Scripts/Items/Fan.cs
@@ -9,6 +9,7 @@ public class Fan : MonoBehaviour
 
     Vector3 forceVector = new Vector3(-10f, 0, -10f);
     public GameObject hamster;
+    public GameObject fanObject;
     public float moveSpeed = 10f;
 
     // Start is called before the first frame update
@@ -25,8 +26,7 @@ public class Fan : MonoBehaviour
 
     private void OnTriggerStay(Collider col)
     {
-        col.gameObject.transform.position += transform.forward * moveSpeed * Time.deltaTime;
-
-        //GetComponent<Rigidbody>().AddRelativeForce(forceVector, ForceMode.Force); 
+       Vector3 hamsterPos = col.gameObject.transform.position;
+       col.gameObject.GetComponent<Rigidbody>().AddRelativeForce(hamsterPos, ForceMode.Force); 
     }
 }
