14993db4bbb4147c33562cc9f2631af7c750981b
Ivory <SIVORY200@caledonian.ac.uk>
2023-03-30 11:12:49 +0100

    new cheese

--- Assets/Scripts/Gameplay/CheeseHealth.cs
+++ Assets/Scripts/Gameplay/CheeseHealth.cs
@@ -1,16 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class CheeseHealth : MonoBehaviour
 {
-    [SerializeField] GameManager gameManager;
+    [SerializeField] GameObject[] cheeseSections;
+    [SerializeField] ParticleSystem cheeseCrumbs;
+    int counter;
 
     private void OnTriggerEnter(Collider col)
     {
         if(col.GetComponent<HamsterBase>() != null)
         {
             col.GetComponent<HamsterBase>().Despawn();
-            gameManager.LoseHealth();
-        }
-        
+            GameManager.instance.LoseHealth();
+            cheeseCrumbs.Play();
+            Destroy(cheeseSections[counter]);
+            counter++;
+
+            if (counter >= cheeseSections.Length)
+            {
+                Destroy(gameObject);
+            }
+        }      
     }
 }
