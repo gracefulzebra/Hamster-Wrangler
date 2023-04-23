daa6fdc0f3dd3df2c5697ffb10047178dbc75d00
Stefan Poys <117182808+Shybbi@users.noreply.github.com>
2023-04-23 20:28:31 +0100

    disintegration update

---
+++ Assets/Scripts/Shaders/DisintegrationShader/DisintegrationController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class DisintegrationController : MonoBehaviour
+{
+    public Animator anim;
+    public VisualEffect VFXgraph;
+    public SkinnedMeshRenderer meshRenderer;
+    public float dissolveRate = 0.05f;
+    public float refreshrate = 0.02f;
+
+    private Material[] disintegrateMaterials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(VFXgraph != null)
+        {
+            VFXgraph.Stop();
+            VFXgraph.gameObject.SetActive(false);
+        }
+
+        if(meshRenderer != null)
+        {
+            disintegrateMaterials = meshRenderer.materials;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Disintegrate());
+        }
+    }
+
+    IEnumerator Disintegrate()
+    {
+        if (anim != null)
+        {
+            anim.SetTrigger("Burn");
+        }
+        yield return new WaitForSeconds(0.2f);
+
+        if(VFXgraph != null)
+        {
+            VFXgraph.gameObject.SetActive(true);
+            VFXgraph.Play();
+        }
+
+        float counter = 0;
+
+        if(disintegrateMaterials.Length > 0)
+        {
+            while(disintegrateMaterials[0].GetFloat("_DissolveAmount") < 1)
+            {
+                counter += dissolveRate;
+
+                for(int i=0; i<disintegrateMaterials.Length; i++)
+                {
+                    disintegrateMaterials[i].SetFloat("_DissolveAmount", counter);
+                }
+                yield return new WaitForSeconds(refreshrate);
+            }
+        }
+        VFXgraph.Stop();
+    }
+}
